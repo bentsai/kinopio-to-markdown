81e50c498af877791492d2e4fe82a6ccc2bfd241
Ben Tsai <betsai@cisco.com>
2021-03-12 10:12:20 -0500

    Add frontmatter

--- ktomd.sh
+++ ktomd.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-curl -s https://api.kinopio.club/space/HASZ8HDbGXn-1jkG-Zud8 | jq -r '.cards | sort_by(.y) | .[] | .name + "\n"'
+curl -s https://api.kinopio.club/space/$1 > kinopio.tmp
+
+echo ---
+echo -n "title: "
+cat kinopio.tmp | jq .name
+echo -n "date: "
+cat kinopio.tmp | jq .updatedAt
+echo "layout: layouts/post.njk"
+
+cat kinopio.tmp | jq -r '.cards | sort_by(.y) | sort_by(.x) | .[] | .name + "\n"'
+
+rm kinopio.tmp
